100f2f621fbca91ae021d485929c5f3023e8fb63
Baptiste <baptiste.nicolas@protonmail.com>
2025-01-07 23:53:25 +0100

    implement itemDelegate

--- treeViewPractice.py
+++ treeViewPractice.py
@@ -4,73 +4,20 @@ from typing import List, Any, Dict, Optional
 from PySide2 import QtWidgets as qtw, QtCore as qtc, QtGui as qtg
 
 
+# view
 class TreeViewPractice(qtw.QTreeView):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setModel(SearchModel())
 
-        data = [
-            {
-                "name": "character",
-                "type": "assetType",
-                "decendent": [
-                    {
-                        "name": "darthPlaguies",
-                        "type": "char",
-                    },
-                    {
-                        "name": "darthSidious",
-                        "type": "char",
-                    },
-                    {
-                        "name": "excavationRobot",
-                        "type": "char",
-                    },
-                ],
-            },
-            {
-                "name": "props",
-                "type": "assetType",
-                "decendent": [
-                    {
-                        "name": "darthPlaguiesLightSaber",
-                        "type": "props"
-                    },
-                    {
-                        "name": "darthSidiousLightSaber",
-                        "type": "props"
-                    },
-                    {
-                        "name": "spaceShip",
-                        "type": "props"
-                    },
-                    {
-                        "name": "drill",
-                        "type": "props"
-                    },
-                ],
-            },
-            {
-                "name": "sets",
-                "type": "assetType",
-                "decendent": [
-                    {
-                        "name": "cave001",
-                        "type": "set"
-                    },
-                    {
-                        "name": "caveSpaceShip",
-                        "type": "set"
-                    },
-                ],
-            },
-        ]
-
         # self.model().sourceModel()
         self.feedModel(data)
         self.model().setRecursiveFilteringEnabled(True)
         self.model().setFilterCaseSensitivity(qtc.Qt.CaseInsensitive)
+
+        self.setItemDelegateForColumn(2, ItemDelegate())
+        self.setEditTriggers(qtw.QAbstractItemView.AllEditTriggers)
 
     def feedModel(
             self,
@@ -78,15 +25,26 @@ class TreeViewPractice(qtw.QTreeView):
             parent: Optional[qtg.QStandardItem] = None
             ) -> None:
 
-        self.model().sourceModel().setHorizontalHeaderLabels(['name', 'type'])
+        self.model().sourceModel().setHorizontalHeaderLabels(['name', 'type', 'assigned'])
         parent = parent or self.model().sourceModel().invisibleRootItem()
 
         for content in data:
             row = parent.rowCount()
             items = []
 
-            for column, dt in enumerate(['name', 'type']):
-                item = qtg.QStandardItem(content.get(dt, 'N/A'))
+            for column, dt in enumerate(['name', 'type', 'assigned']):
+                data = content.get(dt)
+
+                if isinstance(data, str):
+                    item = Item(data)
+
+                elif isinstance(data, list):
+                    item = Item(data[0])
+                    item.setData(data, qtc.Qt.UserRole)
+
+                else:
+                    item = Item('N/A')
+
                 parent.setChild(row, column, item)
                 items.append(item)
 
@@ -94,11 +52,62 @@ class TreeViewPractice(qtw.QTreeView):
                 self.feedModel(childs, items[0])
 
 
+# item delegate (to manage widgets)
+class ItemDelegate(qtw.QItemDelegate):
+
+    def createEditor(
+            self,
+            parent: qtw.QWidget,
+            option: qtw.QStyleOptionViewItem,
+            index: qtc.QModelIndex
+            ) -> qtw.QWidget:
+
+        if index.data(qtc.Qt.UserRole):
+            return qtw.QComboBox(parent)
+
+        else:
+            return super().createEditor(parent, option, index)
+
+    def setEditorData(
+            self,
+            editor: qtw.QWidget,
+            index: qtc.QModelIndex
+            ) -> None:
+
+        if not isinstance(editor, qtw.QComboBox):
+            return super().setEditorData(editor, index)
+
+        data = index.data(qtc.Qt.UserRole)
+        editor.addItems(data)
+        value = index.data()
+
+        if value is not None:
+            editor.setCurrentText(value)
+
+        else:
+            editor.setCurrentIndex(0)
+
+        editor.showPopup()
+
+    def setModelData(
+            self,
+            editor: qtw.QWidget,
+            model: qtc.QAbstractItemModel,
+            index: qtc.QModelIndex
+            ) -> None:
+
+        if not isinstance(editor, qtw.QComboBox):
+            return super().setModelData(editor, model, index)
+
+        model.setData(index, editor.currentText())
+
+
+# proxy model
 class SearchModel(qtc.QSortFilterProxyModel):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.setSourceModel(qtg.QStandardItemModel())
+        self.setSourceModel(ItemModel())
         # self.setRecursiveFilteringEnabled(True)
 
     def setFilterByColumn(self, regex: str, column: int):
@@ -117,6 +126,20 @@ class SearchModel(qtc.QSortFilterProxyModel):
                 return True
 
         return False
+
+
+# item model
+class ItemModel(qtg.QStandardItemModel):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+# item
+class Item(qtg.QStandardItem):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class MainWidget(qtw.QWidget):
@@ -141,6 +164,73 @@ class MainWidget(qtw.QWidget):
 if __name__ == '__main__':
 
     app = qtw.QApplication(sys.argv)
+
+    data = [
+        {
+            "name": "character",
+            "type": "assetType",
+            "decendent": [
+                {
+                    "name": "darthPlaguies",
+                    "type": "char",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "darthSidious",
+                    "type": "char",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "excavationRobot",
+                    "type": "char",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+            ],
+        },
+        {
+            "name": "props",
+            "type": "assetType",
+            "decendent": [
+                {
+                    "name": "darthPlaguiesLightSaber",
+                    "type": "props",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "darthSidiousLightSaber",
+                    "type": "props",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "spaceShip",
+                    "type": "props",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "drill",
+                    "type": "props",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+            ],
+        },
+        {
+            "name": "sets",
+            "type": "assetType",
+            "decendent": [
+                {
+                    "name": "cave001",
+                    "type": "set",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+                {
+                    "name": "caveSpaceShip",
+                    "type": "set",
+                    "assigned": ['bni', 'lpa', 'msh'],
+                },
+            ],
+        },
+    ]
+
     wdg = MainWidget()
     wdg.show()
 
